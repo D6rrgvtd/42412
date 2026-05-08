d43f40616317c201736d3187211639ec47f4db22
raytrek2-\ktc <haru.terada78@gmail.com>
2026-05-08 12:38:33 +0900

    State シーン還暦

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,34 +1,118 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
+public enum StateType
+{
+   Move,
+   Attack
+
+}
+
 public class Player : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] float jumpSpeed;
-
-    PlayerInput playerInput;
-    Rigidbody2D rb;
+    [SerializeField] Animator animator;
     int jumpcount = 0;
     int maxjump = 2;
+   private bool isGrounded;
+    State state;
+    State[] states = new State[]
+     {
+        new StateMove(),
+        new StateAttack(),
+     };
+    StateMove stateMove = new();
+    public PlayerInput playerInput;
+    public Rigidbody2D rb;
+
+    public class State
+    {
+        public PlayerInput playerInput;
+        public Rigidbody2D rb;
+        public Animator animator;
+
+        public virtual void Start() { }
+        public virtual void Update(bool isGrounded, out StateType nextState)
+        {
+            {
+                nextState = StateType.Move;
+            }
+        }
+        public virtual void End() { }
+    }
+    public class StateMove : State
+    {
+        public float speed;
+        public float jumpSpeed;
+
+        public override void Update(bool isGrounded,out StateType nextState)
+        {
+            var move = playerInput.actions["Move"].ReadValue<Vector2>();
+            rb.linearVelocityX = move.x * speed;
+
+            if (playerInput.actions["Jump"].WasPressedThisFrame() && isGrounded)
+            {
+                rb.linearVelocityY = jumpSpeed;
+            }
+            if (playerInput.actions["Attack"].WasPerformedThisFrame())
+            {
+                nextState = StateType.Attack;
+            }
+            else
+            {
+                nextState = StateType.Move;
+            }
+        }
+    }
+    public class StateAttack : State
+    {
+        public override void Start()
+        {
+            animator.Play("Attack");
+        }
+        public override void Update(bool isGrounded, out StateType nextState)
+        {
+            if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+            {
+                nextState = StateType.Move;
+            }
+            else
+            {
+                nextState = StateType.Attack;
+            }
+        }
+    }
+    
+
     void Start()
     {
+       
         playerInput = GetComponent<PlayerInput>();
         rb = GetComponent<Rigidbody2D>();
+        states[(int)StateType.Move].playerInput = playerInput;
+        states[(int)StateType.Move].rb = rb;
+        (states[(int)StateType.Move] as StateMove).speed = speed;
+        (states[(int)StateType.Move] as StateMove).jumpSpeed = jumpSpeed;
+        states[(int)StateType.Move].animator = animator;
+        states[(int)StateType.Attack].playerInput = playerInput;
+        states[(int)StateType.Attack].rb = rb;
+        states[(int)StateType.Attack].animator = animator;
+        state = states[(int)StateType.Move];
+        state.Start();
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        var move = playerInput.actions["Move"].ReadValue<Vector2>();
-
-        rb.linearVelocityX = move.x * speed;
-
-        if (playerInput.actions["Jump"].WasPressedThisFrame() && jumpcount < maxjump)
+        state.Update(isGrounded, out var nextState);
+        if (state != states[(int)nextState])
         {
-            rb.linearVelocityY = jumpSpeed;
-            jumpcount++;
+            state.End();
+            state = states[(int)nextState];
+            state.Start();
         }
-  
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -37,4 +121,5 @@ public class Player : MonoBehaviour
             jumpcount = 0;
         }
     }
+    
 }
